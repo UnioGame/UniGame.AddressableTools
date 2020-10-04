26e1833ed80a10ff99cc6bf2f6323bf86c837584
Mefodei <mefodei.link@gmail.com>
2020-10-05 00:50:09 +0300

    - initialize

---
+++ Runtime/AssetReferencies/ScriptableObjectAssetReferenceT.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UniGreenModules.UniGame.AddressableTools.Runtime.AssetReferencies
+{
+    using System;
+
+    [Serializable]
+    public class ScriptableObjectAssetReferenceT<T> : DisposableAssetReference<T>
+        where T : ScriptableObject
+    {
+        public ScriptableObjectAssetReferenceT(string guid) : base(guid)
+        {
+        }
+    }
+}
