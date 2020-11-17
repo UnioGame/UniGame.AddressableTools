07bf82084b7e9e03e88a24e95b5655f26d834c65
Vorontcov Gleb <vorontcovg@taktikagames.ru>
2020-11-17 15:28:22 +0300

    fixed editorAsset in AssetReferenceComponent

--- Runtime/AssetReferencies/AssetReferenceComponent.cs
+++ Runtime/AssetReferencies/AssetReferenceComponent.cs
@@ -30,6 +30,32 @@ namespace UniModules.UniGame.AddressableTools.Runtime.AssetReferencies
         where TAsset : Component
         where TApi : class
     {
+#if UNITY_EDITOR
+        /// <summary>
+        /// Type-specific override of parent editorAsset.  Used by the editor to represent the main asset referenced.
+        /// </summary>
+        /// <returns>Editor Asset as type TObject, else null</returns>
+        public new TAsset editorAsset
+        {
+            get
+            {
+                if (CachedAsset != null || string.IsNullOrEmpty(AssetGUID) && CachedAsset is GameObject)
+                    return (CachedAsset as GameObject)?.GetComponent<TAsset>();
+                
+                var assetPath = AssetDatabase.GUIDToAssetPath(AssetGUID);
+                var mainType  = AssetDatabase.GetMainAssetTypeAtPath(assetPath);
+                
+                CachedAsset = AssetDatabase.LoadAssetAtPath(assetPath, mainType);
+                
+                var baseAsset = CachedAsset as GameObject;
+                if (baseAsset == null)
+                    return null;
+                
+                return baseAsset.GetComponent<TAsset>();
+            }
+        }
+#endif
+        
         public AssetReferenceComponent(string guid) : base(guid)
         {
         }
