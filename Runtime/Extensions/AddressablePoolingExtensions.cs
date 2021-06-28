33f67108a5b1c6da4766324f942a4918b66fd389
MikhailovSergei <mikhailovs@taktikagames.ru>
2021-06-28 19:16:55 +0300

    - add addressables pooling

---
+++ Runtime/Extensions/AddressablePoolingExtensions.cs
@@ -0,0 +1,40 @@
+using Cysharp.Threading.Tasks;
+using UniModules.UniCore.Runtime.ObjectPool.Runtime.Extensions;
+using UniModules.UniGame.AddressableTools.Runtime.Extensions;
+using UniModules.UniGame.Core.Runtime.DataFlow.Interfaces;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public static class AddressablePoolingExtensions
+{
+
+    public static async UniTask<GameObject> CreatePool(this AssetReferenceT<GameObject> assetReference, ILifeTime lifeTime,int preload = 0)
+    {
+        var source = await assetReference.LoadAssetTaskAsync(lifeTime);
+        source.AttachPoolToLifeTime(lifeTime, true, preload);
+        return source;
+    }
+    
+    public static async UniTask<TComponent> CreatePool<TComponent>(this AssetReferenceT<TComponent> assetReference, ILifeTime lifeTime,int preload = 0)
+        where TComponent : Component
+    {
+        var source = await assetReference.LoadAssetTaskAsync(lifeTime);
+        var sourceObject = source.gameObject;
+        sourceObject.AttachPoolToLifeTime(lifeTime, true, preload);
+        return source;
+    }
+    
+    public static async UniTask<GameObject> Spawn(this AssetReferenceT<GameObject> assetReference, ILifeTime lifeTime)
+    {
+        var source = await assetReference.LoadAssetTaskAsync(lifeTime);
+        return source.Spawn();
+    }
+    
+    public static async UniTask<TComponent> Spawn<TComponent>(this AssetReferenceT<TComponent> assetReference, ILifeTime lifeTime)
+        where TComponent : Component
+    {
+        var source = await assetReference.LoadAssetTaskAsync(lifeTime);
+        return source.Spawn();
+    }
+    
+}
