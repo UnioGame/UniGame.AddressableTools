fe463d40d62288fc052eafd0396fc1dc8c2e29fd
Mefodei <mefodei.link@gmail.com>
2023-02-23 19:19:18 +0700

    - update addressables drawer field

--- Editor/AddressablesAssetDrawer/AddressableAssetInspector.cs
+++ Editor/AddressablesAssetDrawer/AddressableAssetInspector.cs
@@ -3,10 +3,14 @@ using Object = UnityEngine.Object;
 
 namespace UniGame.AddressableTools.Editor
 {
+    using System;
     using System.Collections.Generic;
     using System.Reflection;
+    using Sirenix.OdinInspector.Editor;
+    using Sirenix.Utilities.Editor;
     using UniModules.Editor;
     using UniModules.UniCore.EditorTools.Editor.PropertiesDrawers;
+    using UniModules.UniCore.Runtime.Utils;
     using UniModules.UniGame.AddressableTools.Runtime.Attributes;
     using UniModules.UniGame.Editor.DrawersTools;
     using UnityEditor;
@@ -17,20 +21,35 @@ namespace UniGame.AddressableTools.Editor
     [CustomPropertyDrawer(typeof(DrawAssetReferenceAttribute),true)]
     public class AddressableAssetInspector : PropertyDrawer
     {
-        private const float FieldHeight = 20;
+        private static MemorizeItem<string, Object> AssetsCache = MemorizeTool
+            .Memorize<string, Object>(guid =>
+            {
+                var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                var mainType = AssetDatabase.GetMainAssetTypeAtPath(assetPath);
+                var asset = AssetDatabase.LoadAssetAtPath(assetPath, mainType);
+                return asset;
+            });
+
+        private static MemorizeItem<FieldInfo,PropertyDrawer> DrawersCache = MemorizeTool
+            .Memorize<FieldInfo,PropertyDrawer>(fieldInfo =>
+            {
+                var drawerType = typeof(AssetReference);
+                var drawer     = fieldInfo.GetDrawer(drawerType);
+                return drawer;
+            });
+
+        private const float FieldHeight = 0;
         private const string assetLabel = "Asset";
         private const string guiPropertyName = "m_AssetGUID";
 
-        private static Dictionary<FieldInfo,PropertyDrawer> drawers = new Dictionary<FieldInfo, PropertyDrawer>();
-
-        private bool isFoldoutOpen = false;
+        private static Dictionary<int, bool> _foldouts = new Dictionary<int, bool>();
 
 #if ODIN_INSPECTOR
         private bool odinSupport = true;  
 #else 
         private bool odinSupport = false;
 #endif
-        
+
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) => FieldHeight + base.GetPropertyHeight(property, label);
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
@@ -41,87 +60,67 @@ namespace UniGame.AddressableTools.Editor
                 var targetRect = position;
                 for (int i = 0; i < property.arraySize; i++) {
                     var item = property.GetArrayElementAtIndex(i);
-                    DrawAssetReferenceInspector(targetRect,item,label);
+                    DrawAssetReferenceInspector(targetRect,item,i,label);
                     targetRect.x += GetPropertyHeight(item, label);
                     position.height += targetRect.height;
                     EditorGUILayout.Separator();
                 }
             }
             else {
-                DrawAssetReferenceInspector(position, property, label);
+                DrawAssetReferenceInspector(position, property, 0,label);
             }
 
 
             EditorGUI.EndProperty();
         }
 
-        public void DrawAssetReferenceInspector(Rect position, SerializedProperty property, GUIContent label)
+        public void DrawAssetReferenceInspector(Rect rect, SerializedProperty property,int index, GUIContent label)
         {
-            DrawOnGuiAssetReferenceInspector(position,property);
-
-            DrawAssetReferenceDrawer(position, property, label);
-        }
-
-        private void DrawAssetReferenceDrawer(Rect position, SerializedProperty property, GUIContent label)
-        {
-            if (fieldInfo == null) return;
-
-            position.y      += FieldHeight;
-            position.height =  20f;
-
-            if (!drawers.TryGetValue(fieldInfo,out var drawer)) {
-                var drawerType = typeof(AssetReference);
-                drawer     = fieldInfo.GetDrawer(drawerType);
-                drawers[fieldInfo] = drawer;
-            }
+            _foldouts.TryGetValue(index, out var foldout);
             
-            drawer.OnGUI(position,property,label);
+            foldout = DrawAssetReferenceDrawer(rect, property,foldout, label);
+            
+            DrawOnGuiAssetReferenceInspector(rect,property,foldout);
+
+            _foldouts[index] = foldout;
         }
 
-        public void DrawOnGuiAssetReferenceInspector(Rect position, SerializedProperty property)
+        private bool DrawAssetReferenceDrawer(Rect rect, SerializedProperty property,bool foldout, GUIContent label)
         {
-            position.height = 20;
+            if (fieldInfo == null) return foldout;
 
+            var foldoutRect = rect;
+            foldoutRect.x -= 12;
+            foldout = EditorGUI.Foldout(foldoutRect, foldout, string.Empty);
+            
+            var drawer = DrawersCache[fieldInfo];
+            
+            drawer.OnGUI(rect,property,label);
+
+            return foldout;
+        }
+
+        public void DrawOnGuiAssetReferenceInspector(Rect rect, SerializedProperty property,bool foldout)
+        {
+            if (!foldout) return;
+            
             var guidProperty = property.FindPropertyRelative(guiPropertyName);
             var assetGuid    = guidProperty.stringValue;
-            if (string.IsNullOrEmpty(assetGuid))
-            {
-                EditorDrawerUtils.DrawDisabled(() => {
-                    EditorGUI.ObjectField(position,assetLabel, null, typeof(Object),false); 
-                }); 
-                return;
-            }
             
-            var assetPath = AssetDatabase.GUIDToAssetPath(assetGuid);
-            var mainType  = AssetDatabase.GetMainAssetTypeAtPath(assetPath);
-            var asset     = AssetDatabase.LoadAssetAtPath(assetPath, mainType);
+            if (string.IsNullOrEmpty(assetGuid))
+                return;
+
+            var asset = AssetsCache[assetGuid];
 
             if (!odinSupport)
-                EditorDrawerUtils.DrawDisabled(() => EditorGUI.ObjectField(position,assetLabel, asset, asset != null 
-                    ? asset.GetType() : null,false));    
-            
-            isFoldoutOpen = asset.DrawOdinPropertyWithFoldout(isFoldoutOpen);
-        }
-
-        public void DrawOnGuiAssetReferenceInspector(string assetGuid)
-        {
-            if (string.IsNullOrEmpty(assetGuid))
             {
-                EditorDrawerUtils.DrawDisabled(() => {
-                    EditorGUILayout.ObjectField(assetLabel, null, typeof(Object),false); 
-                }); 
-                return;
+                EditorDrawerUtils.DrawDisabled(() => 
+                    EditorGUI.ObjectField(rect,assetLabel, asset, asset != null 
+                        ? asset.GetType() 
+                        : null,false));
             }
-            
-            var assetPath = AssetDatabase.GUIDToAssetPath(assetGuid);
-            var mainType  = AssetDatabase.GetMainAssetTypeAtPath(assetPath);
-            var asset     = AssetDatabase.LoadAssetAtPath(assetPath, mainType);
 
-            EditorDrawerUtils.DrawDisabled(() => {
-                EditorGUILayout.ObjectField(assetLabel, asset, asset.GetType(),false);
-            }); 
-            
-            isFoldoutOpen = asset.DrawOdinPropertyWithFoldout(isFoldoutOpen);
+            asset.DrawOdinPropertyField(typeof(Object));
         }
 
     }
