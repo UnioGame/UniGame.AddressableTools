b1b9b8b2900640590281a36b261c4e602d87ff83
mefodei <mefodei.link@gmail.com>
2024-03-02 17:39:14 +0700

    - add addressable go collection for loading

---
+++ Runtime/AssetReferencies/AddressableGameObjects.cs
@@ -0,0 +1,32 @@
+﻿namespace Game.Runtime.Services.AddressablesSource
+{
+    using System;
+    using System.Collections.Generic;
+    using Cysharp.Threading.Tasks;
+    using UniGame.AddressableTools.Runtime;
+    using UniGame.Core.Runtime;
+    using Object = UnityEngine.Object;
+
+    [Serializable]
+    public class AddressableGameObjects
+    {
+        public List<AssetReferenceGameObjectValue> items = new();
+        
+        public async UniTask LoadAssets(ILifeTime lifeTime)
+        {
+            var tasks = items.Select(x => LoadContextItem(x, lifeTime));
+            var result = await UniTask.WhenAll(tasks);
+        }
+
+        public async UniTask<Object> LoadContextItem(AssetReferenceGameObjectValue source,ILifeTime lifeTime)
+        {
+            var value = source;
+            var gameObject = await value.LoadAssetTaskAsync(lifeTime);
+            var result = Object.Instantiate(gameObject);
+            if (source.makeImmortal)
+                Object.DontDestroyOnLoad(result);
+
+            return result;
+        }
+    }
+}
---
+++ Runtime/AssetReferencies/AssetReferenceGameObjectValue.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Runtime.Services.AddressablesSource
+{
+    using System;
+    using UnityEngine.AddressableAssets;
+
+    [Serializable]
+    public class AssetReferenceGameObjectValue : AssetReferenceGameObject
+    {
+        public bool makeImmortal;
+
+        public AssetReferenceGameObjectValue(string guid) : base(guid)
+        {
+        }
+    }
+}
